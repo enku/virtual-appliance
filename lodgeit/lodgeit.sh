47ace21ebce8a231a430c2553a4f30cf37235b52
Albert Hopkins <marduk@python.net>
2011-05-12 15:16:20 -0400

    lodgeit: Make opernrc- and stage4- compatible

--- lodgeit/lodgeit.sh
+++ lodgeit/lodgeit.sh
@@ -3,15 +3,15 @@
 set -v
 
 cd ~/
-virtualenv --no-site-packages lodgeitproject
+[ -d lodgeitproject ] || virtualenv --no-site-packages lodgeitproject
 cd lodgeitproject
 source bin/activate
 [ -d lodgeit ] || hg clone http://dev.pocoo.org/hg/lodgeit-main lodgeit
-pip install pygments
-pip install jinja2
-pip install werkzeug
-pip install sqlalchemy
-pip install babel
-pip install pil
-pip install simplejson
+pip install --upgrade pygments
+pip install --upgrade jinja2
+pip install --upgrade werkzeug
+pip install --upgrade sqlalchemy
+pip install --upgrade babel
+pip install --upgrade pil
+pip install --upgrade simplejson
 deactivate
