5e45c56b2fdcd4849e6fcaa9b117423c0e73bef3
Albert Hopkins <marduk@python.net>
2012-05-25 16:35:06 +0000

    teamplayer: Make compatible with django 1.4

--- teamplayer/settings_local.py
+++ teamplayer/settings_local.py
@@ -10,10 +10,15 @@ TP_STREAM_URL = '/stream.mp3'
 `TP_HOME' = 'TP_HOME'
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-DATABASE_ENGINE = 'postgresql_psycopg2'
-DATABASE_NAME = 'teamplayer'
-DATABASE_HOST = ''
-DATABASE_PASSWORD = ''
-DATABASE_PORT = ''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'teamplayer',
+        'HOST': '',
+        'PASSWORD': '',
+        'PORT': ''
+    }
+}
+
 TP_MPD_HOME = 'TP_DB/mpd'
 TP_REPO_URL = '/repo/'
