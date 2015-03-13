05544efe4124f51af992363133cf878a7e67ed8d
Albert Hopkins <marduk@python.net>
2015-03-13 13:49:22 +0000

    kernel.sh: use long option names when calling emerge
    idem.

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -17,7 +17,7 @@ then
     exit
 fi
 
-${EMERGE} ${USEPKG} -Nn sys-kernel/${KERNEL}
+${EMERGE} ${USEPKG} --newuse --noreplace sys-kernel/${KERNEL}
 cp /root/kernel.config /usr/src/linux/.config
 gcc-config 1
 cd /usr/src/linux
